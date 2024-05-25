5039c8c53ca4be95887177b67125d4a00873ef00
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-05-25 00:51:04 -0500

    Added Pokémon objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-alert('Hello world');
-
-// Practice working with variables
-var favoriteFood = 'Pizza';
-document.write('My favorite food is: ' + favoriteFood);
+// Array to hold Pokémon data
+let pokemonList = [
+    // First Pokémon object
+    {
+        name: "Bulbasaur",
+        height: 7,
+        types: ["grass", "poison"]
+    },
+    // Second Pokémon object
+    {
+        name: "Charmander",
+        height: 6,
+        types: ["fire"]
+    },
+    // Third Pokémon object
+    {
+        name: "Squirtle",
+        height: 5,
+        types: ["water"]
+    }
+];
